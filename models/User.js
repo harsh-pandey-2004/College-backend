c950073baa7e7cd7b7cf32bf0b76cbeeefff4bda
Harsh356a <harsh@365aitech.com>
2025-02-05 13:48:07 +0530

    new

--- models/User.js
+++ models/User.js
@@ -9,4 +9,4 @@ const UserSchema = new mongoose.Schema({
     isVerified: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User-college', UserSchema);
